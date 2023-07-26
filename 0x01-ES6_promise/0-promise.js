f17d9ea7296ed7d86e58539a848b6a2136953879
vpnchengo <charo.alvin@gmail.com>
2023-07-26 09:22:04 +0300

    returning a promise

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const data = { message: "This is the API response" };
+      resolve(data);
+    }, 2000);
+  });
+}
+
+export default getResponseFromAPI;
