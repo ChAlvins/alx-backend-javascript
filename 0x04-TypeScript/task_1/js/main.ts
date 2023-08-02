e7bacd0cebec57a75c66642c73b52d598a34c247
vpnchengo <charo.alvin@gmail.com>
2023-08-02 16:36:18 +0300

    printTeacher functions added

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -11,3 +11,9 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 export default Directors;
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher = (firstName: string, lastName: string):string => `${firstName.charAt(0)}. ${lastName}`;
